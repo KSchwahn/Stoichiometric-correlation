419d11b7db77389069478b8f2cfe88992c367ac6
KSchwahn <kevin.schwahn@gmx.de>
2017-05-16 09:13:49 +0200

    Add files via upload

--- Example_Data+Script/Compare_Ara_Ecoli.R
+++ Example_Data+Script/Compare_Ara_Ecoli.R
@@ -1,30 +1,33 @@
+
 # This example reproduces the compraison of A.thalina and E. coli from the manuscript 
 # "Stoichiometric correlation analysis: principles of metabolic functionality from metabolomics data."
-# It will load meatabolomic data from both organism.
-# Afterwards it will calculate all maximal correlations for all possible pairs, triplets and quadruples
-# During the calculation temporary folders and files will be created, as this is more memory efficient
-# Python files are provided to summarize the temporary files and to create files, which are loaded back into the workspace
+
+# It loads the meatabolomics data set of both species, calculates the stoichiometric correlations, 
+# and finally the the maximal correlations for all possible pairs, triplets and quadruples
+# During the calculation temporary folders and files will be created
+# Python files are provided to summarize the temporary files and to create files which are loaded back into the R workspace
 
 # Finally, the following output files are generated
-# A table with statistics for each metabolite, as number of correlations in pairs, triplets and quadruples and the mean and max observed correlation
-# A table comparing, how often each metabolite is part of a significant pair, triplet and quadruple in both data sets
-# A list of overlapping pairs, triplets and quadruples between the two data sets
+# A table including the number of stoichiometric correlations due to pairs, triplets, and quadruples 
+# as well as the mean and max of the stoichiometric correlation for each metabolite
+# A table comparing the coupling degree of metabolites of both species
+# A list of common maximal stoichiometric correlations due to pairs, triplets, and quadruples between the two species
 
 # Preparation Step
 
 # loading data:
-# metabolites in rows and samples/conditions in columns
+# metabolites in rows and conditions/time points in columns
 
 # reading A. thaliana's metabolite data set
 Ara = read.table("Ara_data.txt",sep="\t",header = T)
 # reading E. Coli's metabolite data set
 Ecoli = read.table("Ecoli_data.txt",sep="\t",header = T)
 
-# sourcing R-functions in the corresponding folder folder
+# sourcing R-functions
 file.sources = list.files(path = "../Functions",pattern="/*.R",full.names = T)
 sapply(file.sources,source,.GlobalEnv)
 
-# creating temporary folders before running SCA
+# creating temporary folder before running SCA
 subDir = "Ecoli_run/"
 
 if (file.exists(subDir)){
@@ -37,8 +40,7 @@ if (file.exists(subDir)){
 # Setting input arguments before calling the function ks_stoichiometric_correlation which calculates the stoichiometric correlations (SCs).
 # Data is the metabolomics data set.
 # indices is assigned to the set of {1,2,3,4} as explained in the manuscript. 
-# nblocks, Number of blocks, is set to the number of metabolites. This number will be passed to the function divirors which identifies the 
-# number of blocks into which the data can be divided. Please refer to the user's manual or manuscript for more details. 
+# nblocks, Number of blocks, is set to the number of metabolites.
 # NRcluster shows the number of available nodes for calculating the SCs.
 # Set NRcluster to a value larger than 1 if you have the possibility to run the program in parallel mode.
 NRcluster <- 1
@@ -50,8 +52,9 @@ Ecoli_cor = ks_stoichiometric_correlation(Data = Ecoli,indices = c(1:4), nblocks
 # Setting input arguments before calling the function ks_find_max_cor_tr which estimates all maximal correlation for triplets.
 # triplets are the triplets calculated by ks_stoichiometric_correlation
 # indices is assigned to the set of {1,2,3,4} as explained in the manuscript. 
-# NRcluster1 and NRcluster2 shows the number of available nodes for calculating the SCs. The overall used nodes is the product of NRcluster1 and NRcluster2
-# The function will creat temporary files instead of a R-workspace variable
+# NRcluster1 and NRcluster2 show the number of available nodes for calculating maximal SCs. 
+# The total nodes used for this analysis is the product of NRcluster1 and NRcluster2
+# The function will creat temporary files instead of an R-workspace variable
 ks_find_max_cor_tr(triplets = Ecoli_cor$triplets, indices = c(1:4),NRcluster1 = 1, NRcluster2 = 1)
 
 
@@ -59,9 +62,10 @@ ks_find_max_cor_tr(triplets = Ecoli_cor$triplets, indices = c(1:4),NRcluster1 = 
 # quadruples are the quadruples calculated by ks_stoichiometric_correlation
 # indices is assigned to the set of {1,2,3,4} as explained in the manuscript. 
 # NRcluster shows the number of available nodes for calculating the SCs
-# tr is the correlation threshold, the quadruples should have at least to be considered for the estimation of maximal correlations. A value above 0 decreases the running time
-# The function will creat temporary files instead of a R-workspace variable
-ks_find_max_cor_qu(quadruples=Ecoli_cor$quadruples,c(1:4),NRcluster,tr=0.8)
+# tr is the correlation threshold, the quadruples which satisfy the threshold are considered for detecting the maximal correlations. 
+# A value above 0 decreases the running time
+# The function will creat temporary files instead of an R-workspace variable
+ks_find_max_cor_qu(quadruples=Ecoli_cor$quadruples,indices = c(1:4),NRcluster1 = NRcluster,tr=0.8)
 
 # Preparing to load and combine all temporary files with the provided Python scripts
 command = "python"
@@ -88,29 +92,28 @@ allArgs = c(path2quad,'"Ecoli_quadruples.txt"')
 # args is the combined path and file name
 output = system2(command, args=allArgs, stdout=TRUE)
 
-# Read the created file into a R-workspace variable
+# Read the created file into an R-workspace object
 Ecoli_maxcor_qu = read.table("Ecoli_quadruples.txt",header=T,sep="\t")
 
-# Combine the estimated maximal correlations for the triplets and quadruples in one list. 
-# This is needed, as up following functions expect this as an input
+# Combine the estimated maximal correlations due to triplets and quadruples in one list. 
 Ecoli_MaxCor = list(triplets=Ecoli_maxcor_tr,quadruples=Ecoli_maxcor_qu)
 
 #####
 # Calculate pairwise correlation between metabolites. 
 # Data is the metabolomics data set.
-# log has to be set to TRUE to calculate the correlation based on the maximal correlation
+# log has to be set to TRUE to calculate the stoichiometric correlation
 Ecoli_pair_log=ks_pairwise_cor(Data = Ecoli,log=T)
 
-# Reformate the output for upfollowing analysis
-# Data is list provided as a output by ks_pairwise_cor()
-Ecoli_pair_log_max=ks_find_max_cor(Ecoli_pair_log)
+# Reformate the output for following analysis
+# Data is a list provided as an output of the function ks_pairwise_cor()
+Ecoli_pair_log_max=ks_find_max_cor(Data = Ecoli_pair_log)
 
 # Calcualte standard Pearson correlation.
 # Data is the metabolomics data set.
 Ecoli_pair=ks_pairwise_cor(Data = Ecoli,log=F)
 
 # Reformate the output for upfollowing analysis
-# Data is list provided as a output by ks_pairwise_cor()
+# Data is a list provided as an output of the function ks_pairwise_cor()
 Ecoli_pair_max=ks_find_max_cor(Data = Ecoli_pair)
 
 ##########################################################
@@ -130,8 +133,7 @@ if (file.exists(subDir)){
 # Setting input arguments before calling the function ks_stoichiometric_correlation which calculates the stoichiometric correlations (SCs).
 # Data is the metabolomics data set.
 # indices is assigned to the set of {1,2,3,4} as explained in the manuscript. 
-# nblocks, Number of blocks, is set to the number of metabolites. This number will be passed to the function divirors which identifies the 
-# number of blocks into which the data can be divided. Please refer to the user's manual or manuscript for more details. 
+# nblocks, Number of blocks, is set to the number of metabolites.
 # NRcluster shows the number of available nodes for calculating the SCs.
 # Set NRcluster to a value larger than 1 if you have the possibility to run the program in parallel mode.
 NRcluster <- 1
@@ -143,16 +145,18 @@ Ara_cor = ks_stoichiometric_correlation(Data=Ara,indices=c(1:4),nblocks=nrow(Ara
 # Setting input arguments before calling the function ks_find_max_cor_tr which estimates all maximal correlation for triplets.
 # triplets are the triplets calculated by ks_stoichiometric_correlation
 # indices is assigned to the set of {1,2,3,4} as explained in the manuscript. 
-# NRcluster1 and NRcluster2 shows the number of available nodes for calculating the SCs. The overall used nodes is the product of NRcluster1 and NRcluster2
-# The function will creat temporary files instead of a R-workspace variable
+# NRcluster1 and NRcluster2 show the number of available nodes for calculating maximal SCs. 
+# The total nodes used for this analysis is the product of NRcluster1 and NRcluster2
+# The function will creat temporary files instead of an R-workspace variable
 ks_find_max_cor_tr(triplets=Ara_cor$triplets,indices=c(1:4),NRcluster1=1,NRcluster2 = 1)
 
 # Setting input arguments before calling the function ks_find_max_cor_qu which estimates all maximal correlation for quadruples.
 # quadruples are the quadruples calculated by ks_stoichiometric_correlation
 # indices is assigned to the set of {1,2,3,4} as explained in the manuscript. 
 # NRcluster shows the number of available nodes for calculating the SCs
-# tr is the correlation threshold, the quadruples should have at least to be considered for the estimation of maximal correlations. A value above 0 decreases the running time
-# The function will creat temporary files instead of a R-workspace variable
+# tr is the correlation threshold, the quadruples which satisfy the threshold are considered for detecting the maximal correlations. 
+# A value above 0 decreases the running time
+# The function will creat temporary files instead of an R-workspace variable
 ks_find_max_cor_qu(quadruples=Ara_cor$quadruples,indices=c(1:4),NRcluster,tr=0.8)
 
 
@@ -169,7 +173,7 @@ allArgs = c(path2trip,'"Ara_triplets.txt"')
 # args is the combined path and file name
 output = system2(command, args=allArgs, stdout=TRUE)
 
-# Read the created file into a R-workspace variable
+# Read the created file into an R-workspace object
 Ara_maxcor_tr = read.table("Ara_triplets.txt",header=T,sep = "\t")
 
 # Define the path to the quadruple Python script
@@ -181,28 +185,28 @@ allArgs = c(path2quad,'"Ara_quadruples.txt"')
 # command calls Python within the terminal
 # args is the combined path and file name
 output = system2(command, args=allArgs, stdout=TRUE)
-# Read the created file into a R-workspace variable
+# Read the created file into an R-workspace variable
 Ara_maxcor_qu = read.table("Ara_quadruples.txt",header=T,sep="\t")
 
-# Combine the estimated maximal correlations for the triplets and quadruples in one list. 
-# This is needed, as up following functions expect this as an input
+# Combine the estimated maximal correlations due to triplets and quadruples in one list. 
 Ara_MaxCor = list(triplets=Ara_maxcor_tr,quadruples=Ara_maxcor_qu)
 
 #####
 # Calculate pairwise correlation between metabolites. 
 # Data is the metabolomics data set.
-# log has to be set to TRUE to calculate the correlation based on the maximal correlation
+# log has to be set to TRUE to calculate the stoichiometric correlation
 Ara_pair_log=ks_pairwise_cor(Data=Ara,log=T)
 
-# Reformate the output for upfollowing analysis
-# Data is list provided as a output by ks_pairwise_cor()
+# Reformate the output for following analysis
+# Data is a list provided as an output of the function ks_pairwise_cor()
 Ara_pair_log_max=ks_find_max_cor(Data=Ara_pair_log)
 
 # Calcualte standard Pearson correlation.
 # Data is the metabolomics data set.
 Ara_pair=ks_pairwise_cor(Data=Ara,log=F)
+
 # Reformate the output for upfollowing analysis
-# Data is list provided as a output by ks_pairwise_cor()
+# Data is a list provided as an output of the function ks_pairwise_cor()
 Ara_pair_max=ks_find_max_cor(Data=Ara_pair)
 
 #move back to the main directory to write the output files
@@ -216,8 +220,7 @@ setwd("../")
 # pair is a data.frame with the maximal correlated pairs generated by ks_find_max_cor
 # Corr is a list with the maximal correlated triplets and quadruples generated by ks_find_max_cor_tr and ks_find_max_cor_qu
 # Names are the names of the metabolites 
-# tr is the correlation threshold, the pairs, triplets and quadruples should have at least to be considered for the output
-# Repeat the function call for each data set and threshold
+# tr is the correlation threshold, the pairs, triplets, and quadruples should satisfy the threshold to be considered in the output
 
 write.table(ks_make_table(pair=Ecoli_pair_log_max,Corr=Ecoli_MaxCor,Names=rownames(Ecoli),tr=0.8),file = "Ecoli_complete_table_08.tab",row.names=F,col.names=T,quote = F,sep="\t")
 write.table(ks_make_table(pair=Ecoli_pair_log_max,Corr=Ecoli_MaxCor,Names=rownames(Ecoli),tr=0.85),file = "Ecoli_complete_table_085.tab",row.names=F,col.names=T,quote = F,sep="\t")
@@ -228,14 +231,14 @@ write.table(ks_make_table(pair=Ara_pair_log_max,Corr=Ara_MaxCor,Name=rownames(Ar
 write.table(ks_make_table(pair=Ara_pair_log_max,Corr=Ara_MaxCor,Names=rownames(Ara),tr=0.9),file = "Ara_complete_table_09.tab",row.names=F,col.names=T,quote = F,sep="\t")
 
 
-############################################################
-# Procede from here if you want to compare two data sets:
+############################################################    Comparative analysis
+# Proceed from here if you want to compare the two species:
 
 # ks_make_bipartite_graph creates a bipartite graph using the igraph package
 # pairs is a data.frame with the maximal correlation of pairs generated by ks_find_max_cor
 # triplets is a data.frame maximal correlated triplets generated by ks_find_max_cor_tr
 # quadruples is a data.frame maximal correlated quadruples generated by ks_find_max_cor_qu
-# tr is the correlation threshold, the pairs, triplets and quadruples should have at least to be considered for the output
+# tr is the correlation threshold, the pairs, triplets, and quadruples should satisfy the threshold to be considered in the output
 
 Ecoli_bipartite_graph_08 = ks_make_bipartite_graph(pairs=Ecoli_pair_log_max,triplets=Ecoli_MaxCor$triplets,quadruples=Ecoli_MaxCor$quadruples,tr=0.8)
 Ecoli_bipartite_graph_085 = ks_make_bipartite_graph(pairs=Ecoli_pair_log_max,triplets=Ecoli_MaxCor$triplets,quadruples=Ecoli_MaxCor$quadruples,tr=0.85)
@@ -246,7 +249,7 @@ Ara_bipartite_graph_085 = ks_make_bipartite_graph(pairs=Ara_pair_log_max,triplet
 Ara_bipartite_graph_09 = ks_make_bipartite_graph(pairs=Ara_pair_log_max,triplets=Ara_MaxCor$triplets,quadruples=Ara_MaxCor$quadruples,tr=0.9)
 
 # The function takes the bipartite graphs from the function ks_make_bipartite_graph from two different data set and creates an appropriate output,
-# listing the metabolites and their number of appearences in paris, triplets and quadruples
+# listing the metabolites and their corresponding coupling degree
 # graph1: a bipartite graph of the first data set, generated by ks_make_bipartite_graph
 # graph2: a bipartite graph of the second data set, generated by ks_make_bipartite_graph
 # name1: the names of the metabolites of the first data set
@@ -267,7 +270,7 @@ write_list(out_list,wb_name = "Metabolite_coupling_degree.xlsx")
 
 ###############################
 
-# ks_shared_metabolites creates a list showing the overlapping pairs, triplets and quadruples at the choosen threshold
+# ks_shared_metabolites creates a list presenting the overlapping pairs, triplets and quadruples at the choosen threshold
 # data_pair is a data.frame with the maximal correlation of pairs generated by ks_find_max_cor of the first data set
 # data_all is a list with the maximal correlated triplets and quadruples generated by ks_find_max_cor_tr and ks_find_max_cor_qu of the firs data set
 # data2_pair is a data.frame with the maximal correlation of pairs generated by ks_find_max_cor of the second data set
